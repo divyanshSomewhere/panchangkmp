0ab62bfb484ef513b324779e26754998155e8e9e
divyansh <divyansh@chalo.com>
2025-03-13 19:09:44 +0530

    - Boilerplate code setup for android - ios not be working yet, will require some tweaks

--- composeApp/src/iosMain/kotlin/com/gometro/core/di/SharedCoreModule.ios.kt
+++ composeApp/src/iosMain/kotlin/com/gometro/core/di/SharedCoreModule.ios.kt
@@ -1,6 +1,15 @@
 package com.gometro.core.di
 
-import org.koin.core.module.Module
+import com.gometro.base.providers.toast.KotlinToastManager
+import com.gometro.base.providers.toast.KotlinToastManagerImpl
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
 
 
-internal actual fun platformModule() = Module()
+internal actual fun platformModule() = module {
+
+    singleOf(::KotlinToastManagerImpl) { bind<KotlinToastManager>() }
+
+
+}
